4f9e94a466baac14150637fc8cdcbbc19c820986
piales00 <perez.izquierdo.piero@gmail.com>
2025-02-07 23:41:57 -0500

    Creación 1.0 del formulario y de sus controladores

--- Panaderia/Panaderia/Controllers/FormController.cs
+++ Panaderia/Panaderia/Controllers/FormController.cs
@@ -3,15 +3,57 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Configuration;
+using MySql.Data.MySqlClient;
+using Panaderia.Models;
+
 
 namespace Panaderia.Controllers
 {
     public class FormController : Controller
     {
-        // GET: Form
-        public ActionResult Index()
+        public ActionResult Crear()
         {
             return View();
         }
+
+        [HttpPost]
+        public ActionResult Crear(Form formulario)
+        {
+            if (ModelState.IsValid)
+            {
+                string connectionString = ConfigurationManager.ConnectionStrings["MySqlConnection"].ConnectionString;
+
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
+                {
+                    try
+                    {
+                        connection.Open();
+
+                        string query = "INSERT INTO Formulario (Nombre, Correo, Mensaje, Fecha_envio) VALUES (@Nombre, @Correo, @Mensaje, @FechaEnvio)";
+
+                        using (var command = new MySqlCommand(query, connection))
+                        {
+                            command.Parameters.AddWithValue("@Nombre", formulario.Nombre);
+                            command.Parameters.AddWithValue("@Correo", formulario.Correo);
+                            command.Parameters.AddWithValue("@Mensaje", formulario.Mensaje);
+                            command.Parameters.AddWithValue("@FechaEnvio", DateTime.Now); 
+
+                            command.ExecuteNonQuery();
+                        }
+
+                        return RedirectToAction("Crear");
+                    }
+                    catch (Exception ex)
+                    {
+                        ModelState.AddModelError("", "Ocurrió un error al enviar el formulario: " + ex.Message);
+                    }
+                }
+            }
+
+            return View(formulario);
+        }
+
     }
+
 }
---
+++ Panaderia/Panaderia/Models/Form.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Panaderia.Models
+{
+    public class Form
+    {
+        public int IdFormulario { get; set; }
+        public string Nombre { get; set; }
+        public string Correo { get; set; }
+        public string Mensaje { get; set; }
+        public DateTime? FechaEnvio { get; set; }
+    }
+}
